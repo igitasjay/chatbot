5616870cf50175c2dc01ad0e32ed10808ba38126
Justice Philemon <imailasjay>
2024-08-20 03:18:25 +0100

    version 1.

---
+++ lib/home.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<ChatMessage> messages = [];
+  Gemini gemini = Gemini.instance;
+  ChatUser currentUser = ChatUser(id: "0", firstName: "Me");
+  ChatUser geminiAI = ChatUser(
+    id: "1",
+    firstName: "Bot",
+    // profileImage: "https://images.app.goo.gl/zektCg5QcZnEPUmz8",
+  );
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Bot"),
+            SizedBox(width: 8),
+            Icon(Icons.mode_night_sharp),
+          ],
+        ),
+      ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return DashChat(
+      inputOptions: InputOptions(
+        textController: controller,
+        trailing: [
+          IconButton(
+            onPressed: _sendMediaImage,
+            icon: const Icon(Icons.image),
+          ),
+        ],
+      ),
+      currentUser: currentUser,
+      onSend: _sendMessage,
+      messages: messages,
+    );
+  }
+
+  void _sendMessage(ChatMessage chatMessage) {
+    setState(() {
+      messages = [chatMessage, ...messages];
+    });
+    try {
+      String question = chatMessage.text;
+      List<Uint8List>? images;
+      if (chatMessage.medias?.isNotEmpty ?? false) {
+        images = [
+          File(chatMessage.medias!.first.url).readAsBytesSync(),
+        ];
+      }
+      gemini.streamGenerateContent(question, images: images).listen((event) {
+        ChatMessage? lastMessage = messages.firstOrNull;
+        if (lastMessage != null && lastMessage.user == geminiAI) {
+          lastMessage = messages.removeAt(0);
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          lastMessage.text += response;
+          setState(() {
+            messages = [lastMessage!, ...messages];
+          });
+        } else {
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          ChatMessage message = ChatMessage(
+            user: geminiAI,
+            createdAt: DateTime.now(),
+            text: response,
+          );
+          setState(() {
+            messages = [message, ...messages];
+          });
+        }
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void _sendMediaImage() async {
+    ImagePicker picker = ImagePicker();
+    XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      ChatMessage chatMessage = ChatMessage(
+        user: currentUser,
+        createdAt: DateTime.now(),
+        text: controller.text,
+        medias: [
+          ChatMedia(
+            url: file.path,
+            fileName: "",
+            type: MediaType.image,
+          ),
+        ],
+      );
+      _sendMessage(chatMessage);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:chatbot/home.dart';
+import 'package:chatbot/secret.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 void main() {
+  Gemini.init(apiKey: Chatbot.GEMINI_API_KEY);
   runApp(const MyApp());
 }
 
@@ -9,8 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chatbot',
+      home: Home(),
     );
   }
 }
